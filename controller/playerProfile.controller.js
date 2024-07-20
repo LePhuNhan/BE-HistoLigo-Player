9693b70c8052f6cb56c16856bd5fc903fa5fd5c9
LePhuNhan <lephunhan2401@gmail.com>
2024-07-20 21:00:11 +0700

    PlayerProfile

--- controller/playerProfile.controller.js
+++ controller/playerProfile.controller.js
@@ -21,3 +21,15 @@ export const getPlayerProfile = async (req, res, next) => {
     res.status(500).json({ message: error.message });
   }
 };
+export const updatePlayerProfile = async (req, res, next) => {
+    try {
+      const player = await PlayerModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!player) {
+        return res.status(404).json({ message: "Player not found" });
+      }
+      res.status(200).json({ message: "Player updated successfully", player });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+};
+  
--- models/player.model.js
+++ models/player.model.js
@@ -1,33 +1,68 @@
+import { Maximize } from "@mui/icons-material";
 import mongoose from "mongoose";
 
 const PlayerSchema = mongoose.Schema({
   playerId: {
     type: mongoose.Types.ObjectId,
     ref: "player",
+    maxlength:100
   },
-  fullname: String,
-  dateOfBirth: Date,
+  fullname: {
+    type:String,
+  },
+  dateOfBirth: {
+    type:Date,
+  },
   email: {
     type: String,
     required: true,
     unique: true,
+    maxlength:250
   },
-  phoneNumber: String,
+  phoneNumber: {
+    type: String,
+    maxlength:20
+  },
   userName: {
     type: String,
     required: true,
+    maxlength:250
   },
   password: {
     type: String,
     required: true,
+    maxlength:250
   },
-  avatar: String,
-  totalScore: Number,
-  totalTime: Number,
-  rank: Number,
-  registrationDate: Date,
-  status: Number,
-  locale: String
+  avatar: {
+    type:String,
+    maxlength:1000
+  },
+  sex: {
+    type: Number,
+    enum: [0,1]
+  },
+  totalScore: {
+    type:Number,
+  },
+  totalTime: {
+    type:Number,
+  },  
+  rank: {
+    type:Number,
+    enum: [0,1,2,3]
+  },
+  registrationDate: {
+    type:Date,
+  },
+  status: {
+    type: Number,
+    enum: [0,1]
+  },
+  locale: {
+    type:String,
+    maxlength:20
+  }
+    
 });
 
 const PlayerModel = mongoose.model("player", PlayerSchema);
