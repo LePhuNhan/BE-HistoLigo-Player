a0c561143d3c9df8696d33e839ec7912324259af
LePhuNhan <lephunhan2401@gmail.com>
2024-09-04 00:29:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,20 @@ dotenv.config();
 const app = express();
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+
+// const allowlist = ['http://localhost:3000', 'https://histolingo-player-fe.cd95dznd.xyz']
+// const corsOptionsDelegate = function (req, callback) {
+//   let corsOptions = {};
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
+// app.use(cors(corsOptionsDelegate));
+
+app.use(cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
